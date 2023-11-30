d4f504a92110790b1e8a7ba92fd7e23f8c94a62b
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2023-11-30 16:01:24 +0000

    change scripts and styles in welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,12 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="stylesheet" href="{{ asset('public/dist/assets/index.88c98a4c.css') }}">
         <title>TCG Scheduling App</title>
 
     </head>
     <body class="antialiased">
         <div id="root"></div>
-        <script src="{{ asset('path/to/your/react/bundle.js') }}"></script>
+        <script src="{{ asset('public/dist/assets/index.eb866770.js') }}"></script>
     </body>
 </html>
