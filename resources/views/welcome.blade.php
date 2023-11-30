8bf6fcaff2ae21f482453d22865332308160c7e6
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2023-11-30 17:39:41 +0000

    add new vite files

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,12 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="{{ asset('public/dist/assets/index.88c98a4c.css') }}">
+        <link rel="stylesheet" href="{{ asset('dist/assets/index.88c98a4c.css') }}">
         <title>TCG Scheduling App</title>
 
     </head>
     <body class="antialiased">
         <div id="root"></div>
-        <script src="{{ asset('public/dist/assets/index.eb866770.js') }}"></script>
+        <script src="{{ asset('dist/assets/index.411e1d4d.js') }}"></script>
     </body>
 </html>
