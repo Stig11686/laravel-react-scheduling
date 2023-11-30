b8bb2ca22097246ef0629d8141d56fada017a1b6
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2023-11-30 15:29:08 +0000

    adding new version to integrate react

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{any}', function () {
+    return view('welcome'); // Assuming 'react_app' is the name of your Blade view for React
+})->where('any', '.*');
